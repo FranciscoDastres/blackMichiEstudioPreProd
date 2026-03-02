759079d5004bccbe78d4e4a174ca80598595d25f
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-03-02 05:48:36 -0300

    fix: :bug: modify axios for bug un pre prod

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-// Interceptor para enviar token automáticamente
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
 
