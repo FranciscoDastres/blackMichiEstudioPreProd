c2f9620c669d6d929ab4d00bcdbfacfc4a6d7615
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-03-18 22:09:43 -0300

    feat: :sparkles: deleting obsolet files

--- backend/db/db.js
+++
@@ -1,6 +0,0 @@
-// const { Pool } = require('pg');
-// const pool = new Pool({
-//   connectionString: process.env.PG_URI || 'postgresql://postgres:hola123@localhost:5432/blackmichiestudio'
-// });
-// module.exports = pool;
- 
