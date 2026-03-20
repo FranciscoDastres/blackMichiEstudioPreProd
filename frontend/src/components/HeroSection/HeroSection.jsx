d8243cb341af96af2e4c28febeb721e155e4a119
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-03-20 02:24:55 -0300

    feat: :art: adding opacity to herosection image

--- frontend/src/components/HeroSection/HeroSection.jsx
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -91,7 +91,7 @@ export default function HeroSection() {
                                     aria-hidden="true"
                                     fetchPriority={index === 0 ? "high" : "auto"}
                                     loading={index === 0 ? "eager" : "lazy"}
-                                    className="absolute inset-0 w-full h-full object-cover opacity-65 scale-110"
+                                    className="absolute inset-0 w-full h-full object-cover opacity-90 scale-110"
                                     style={{ objectPosition: 'right center' }}
                                 />
 
