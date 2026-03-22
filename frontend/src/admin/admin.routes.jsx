93c0d0173d78c2c001e1c3695f785e7a0a809357
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-03-22 07:27:18 -0300

    fix: :bug: working fixing bug with users in admin view

--- frontend/src/admin/admin.routes.jsx
+++ frontend/src/admin/admin.routes.jsx
@@ -1,8 +1,8 @@
 import AdminLayout from "./layout/AdminLayout";
 import Dashboard from "./pages/Dashboard";
-import Products from "./pages/Products";
+import ProductsPage from "./pages/Products/ProductsPage"; // ← archivo correcto
 import Settings from "./pages/Settings";
 import Orders from "./pages/Orders";
 import Users from "./pages/Users";
 
-export { Dashboard, Products, Settings, Orders, Users, AdminLayout };
+export { Dashboard, ProductsPage as Products, Settings, Orders, Users, AdminLayout };
