5387055b314dec21948c4c7a5c31214883c440a0
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-03-22 07:39:50 -0300

    remove ErrorBoundary debug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,36 +11,14 @@ import "./index.css";
 initHoverImageLoading();
 initIntersectionObserver();
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { error: null, info: null };
-  }
-  componentDidCatch(error, info) {
-    this.setState({ error, info });
-  }
-  render() {
-    if (this.state.error) {
-      return (
-        <pre style={{ color: "#fff", background: "#111", padding: "20px", fontSize: "12px", overflow: "auto" }}>
-          {`ERROR: ${this.state.error?.message}\n\nCOMPONENT STACK:\n${this.state.info?.componentStack}`}
-        </pre>
-      );
-    }
-    return this.props.children;
-  }
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <AuthProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </ErrorBoundary>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <AuthProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
