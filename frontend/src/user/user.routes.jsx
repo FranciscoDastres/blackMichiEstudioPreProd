6e8649dd415dedf1122dba39fdac7236c273637c
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-04-05 14:45:42 -0400

    fix: :bug: working with google auth

--- frontend/src/user/user.routes.jsx
+++ frontend/src/user/user.routes.jsx
@@ -5,6 +5,7 @@ import Profile from './pages/Account/Profile';
 import Security from './pages/Account/Security';
 import OrdersPage from './pages/Orders/OrdersPage';
 import OrderDetail from './pages/Orders/OrderDetail';
+import ReviewsPage from './pages/Reviews/ReviewsPage';
 
 export default function UserRoutes() {
     return (
@@ -15,6 +16,7 @@ export default function UserRoutes() {
                 <Route path="seguridad" element={<Security />} />
                 <Route path="pedidos" element={<OrdersPage />} />
                 <Route path="pedidos/:id" element={<OrderDetail />} />
+                <Route path="resenas" element={<ReviewsPage />} />
             </Route>
         </Routes>
     );
