0be936856a1378ab7697d1882d5eef717c9a1a10
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-04-07 15:13:22 -0400

    feat: :art: adding imports for toast and modify some colors in toaster

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Toaster } from "sonner";
 import App from "./App";
 import { CartProvider } from "./contexts/CartContext";
 import { AuthProvider } from "./contexts/AuthContext";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 import { initHoverImageLoading, initIntersectionObserver } from "./utils/lazyImageLoader";
 import "./index.css";
 
@@ -16,14 +18,29 @@ const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <AuthProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <ErrorBoundary>
+      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+          <AuthProvider>
+            <CartProvider>
+              <App />
+              <Toaster
+                position="top-right"
+                richColors
+                closeButton
+                theme="dark"
+                toastOptions={{
+                  style: {
+                    background: "rgba(20, 20, 20, 0.95)",
+                    border: "1px solid rgba(255, 255, 255, 0.1)",
+                    color: "#fff",
+                  },
+                }}
+              />
+            </CartProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
