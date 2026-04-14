7633ff65727af6f68278387f4b764905b9f3e578
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-04-14 17:18:32 -0400

    feat: :sparkles: Init de Sentry condicional a VITE_SENTRY_DSN; llamadas a initAnalytics() y reportWebVitals()

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,27 @@ import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
+import * as Sentry from "@sentry/react";
 import App from "./App";
 import { CartProvider } from "./contexts/CartContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import { FavoritesProvider } from "./contexts/FavoritesContext";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 import { initHoverImageLoading, initIntersectionObserver } from "./utils/lazyImageLoader";
+import { initAnalytics, reportWebVitals } from "./utils/analytics";
 import "./index.css";
 
+if (import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    environment: import.meta.env.MODE,
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: Number(import.meta.env.VITE_SENTRY_TRACES_SAMPLE_RATE ?? 0.1),
+  });
+}
+
+initAnalytics();
+reportWebVitals();
 initHoverImageLoading();
 initIntersectionObserver();
 
