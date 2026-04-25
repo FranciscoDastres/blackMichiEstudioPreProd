7e8401aaa186bfab651d3f3e4926285fb65033eb
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-04-25 01:05:57 -0400

    feat: :zap: Migra TopBanner.jsx a TSX

---
+++ frontend/src/components/TopBanner/TopBanner.tsx
@@ -0,0 +1,8 @@
+// TopBanner.tsx
+export default function TopBanner() {
+  return (
+    <div className="w-full bg-primary text-primary-foreground px-2 py-1 text-center text-xs md:text-sm font-medium tracking-wide shadow-sm">
+      Impresiones 3D personalizadas y envíos a todo el país! 🚚✨
+    </div>
+  );
+}
