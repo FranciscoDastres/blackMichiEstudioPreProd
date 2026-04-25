8dbfb5d357b46664904e76be44fb58b9a56889a1
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-04-25 01:02:07 -0400

    feat: :zap: Migra Layout.jsx a TSX con { children: ReactNode }

---
+++ frontend/src/components/Layout/Layout.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+import TopBanner from "../TopBanner/TopBanner";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { useLocation } from "react-router-dom";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+function Layout({ children }: LayoutProps) {
+  const location = useLocation();
+  const isLogin = location.pathname === "/login";
+
+  return (
+    <div className="min-h-screen font-sans bg-background flex flex-col">
+      <div className="h-[40px] shrink-0">
+        {!isLogin && <TopBanner />}
+      </div>
+      {!isLogin && <Header />}
+      <main className="bg-background flex-1">
+        {children}
+      </main>
+      {!isLogin && <Footer />}
+    </div>
+  );
+}
+
+export default Layout;
