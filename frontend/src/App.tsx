a0a16d12e7c150176268cd0a9bdd5e21929efc08
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-04-25 14:19:48 -0400

    feat: :zap: Creado (migración desde JSX)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import AppRoutes from "./routes/AppRoutes";
+
+function App() {
+  return (
+    <main className="min-h-screen w-full bg-background">
+      <AppRoutes />
+    </main>
+  );
+}
+
+export default App;
